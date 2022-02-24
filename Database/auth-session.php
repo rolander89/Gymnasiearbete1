d7eee98802b207bb9b064ddfc80775e3d66f9f19
rolander89 <100342853+rolander89@users.noreply.github.com>
2022-02-24 14:40:31 +0100

    Delete auth-session.php

--- Database/auth-session.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if(!isset($_SESSION["username"])) {
-            echo "<script>alert('Logga in');";
-			echo 'window.location.href  = "1Framsida.html"';
-		echo '</script>';
-        exit();
-    }
-?>
