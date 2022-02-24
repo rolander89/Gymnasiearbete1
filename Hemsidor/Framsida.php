05e8592e02274898cc8ffa95905d9a0125b92183
rolander89 <100342853+rolander89@users.noreply.github.com>
2022-02-24 09:16:08 +0100

    Create Framsida.php

---
+++ Hemsidor/Framsida.php
@@ -0,0 +1,50 @@
+<?php
+
+include("auth_session.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>NIRO</title>
+  <link rel="stylesheet" href="1framsida.css" />
+</head>
+<body>
+ <div id="Klassrum"><img id="klass" src="klass.jpg"></div>
+  <div id="header">
+    <ul>
+     <li><a href="Framsida.php">Startsida</a></li>
+     <li onclick="location.href='2nyttklassrum.php'"><a href="#Nyttklassrum">Nytt klassrum</a></li>
+	 <li onclick="location.href=''"><a href="#Minaklassrum">Mina klassrum</a></li>
+     <button onclick="location.href= 'loggaut.php'" style="width:auto;">Log out</button>
+
+
+     
+	 <li style="float:right"><a class="active" href="#Hjälp">Hjälp</a></li>
+    </ul>
+
+      
+	   <div id="Titel"><center><h1>Välkommen till din virtuella klass</h1></center> </div>
+	   <div id="container">
+	   <iframe src="The Sidemen falling over for 3 minutes straight.mp4" name="iframe"></miniframe></div><br />
+	   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	   
+	   <script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+
+</body>	   
+</html>
+  <iframe src="The Sidemen falling over for 3 minutes straight.mp4" name="iframe"></miniframe></div><br />
+  
+  
+  
