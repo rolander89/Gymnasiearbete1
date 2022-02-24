f743b9452c55b1d5e7c1f8c263b213cf02678730
rolander89 <100342853+rolander89@users.noreply.github.com>
2022-02-24 14:36:19 +0100

    Create loggaut.php

---
+++ Login/loggaut.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+
+		session_destroy();
+echo "<script>alert('You are now logged out');";
+			echo 'window.location.href  = "1Framsida.html"';
+		echo '</script>';
+
+
+?>
